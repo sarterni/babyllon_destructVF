9aad942cfd99d8101d0696cee50f0a41ead5eda5
sarterni <sarter.nicolas@gmail.com>
2024-03-26 20:43:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ window.addEventListener('load', function () {
 
 
     let images = ['Babylon Destruct copy.png', 'guitar-756326.jpg', 'diapo1.jpg', 'diapo2.jpg', 'diapo3.jpg', 'diapo4.jpg'];
-    let texts = ['logo', 'guitar-756326.jpg',  'diapo1.jpg', 'diapo2.jpg', 'diapo3.jpg', 'diapo4.jpg'];
+    let texts = ['logo', 'guitar-756326.jpg', 'diapo1.jpg', 'diapo2.jpg', 'diapo3.jpg', 'diapo4.jpg'];
     let i = 0;
     let diapo = document.getElementById('img');
     let textDiapo = document.getElementById('places');
-    
+
     let timer = setInterval(function () {
         diapo.src = '../img/diapo/' + images[i];
         textDiapo.innerHTML = texts[i];
@@ -15,7 +15,7 @@ window.addEventListener('load', function () {
         if (i == images.length) i = 0;
     }, 5000);
 
-    
+
 
 
 
